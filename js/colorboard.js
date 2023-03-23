278359fe398e071861c651881af5cdcf1278ce7a
Finn Poduska <111396997+ImThatOneGuyInTheCorner@users.noreply.github.com>
2023-03-23 21:08:08 +0000

    Thu, Mar 23, 2023, 2:08 PM -07:00

---
+++ js/colorboard.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('container')
+const colors = ['red', 'yellow', 'green', 'blue', 'purple', 'orange', 'pink']
+const SQUARES = 500
+
+for (let i = 0; i < SQUARES; i++) {
+    const SQUARES = doocument.createElement('div')
+    square.classList.add('square')
+    container.appendChild(square)
+    square.addEventListener('mouseover', () => setColor(square))
+}
+
+function setColor(square) {
+    square.style.background = 
+}
